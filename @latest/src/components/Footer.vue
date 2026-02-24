e98227681e136c31697ae986d906b4790cca99f3
SachaCorminboeuf <160543645+SachaCorminboeuf@users.noreply.github.com>
2026-02-24 16:24:07 -0500

    Création des fichiers: Footer.vue,Header.vue,MainScreen.vue et AddItem.vue ainsi que le logo

---
+++ @latest/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+<template>
+<h1>
+    <p>Copyright © 2026 Sacha Ostiguy-Corminboeuf</p>
+</h1>
+</template>
+<style scoped>
+</style>
---
+++ @latest/src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+<template>
+<h1>
+    <img src="/src/assets/images/image.png" alt="Logo Path of Exile" />
+    <p>Path Of Exile Trading Hall</p>
+</h1>
+</template>
+<style scoped>
+</style>
